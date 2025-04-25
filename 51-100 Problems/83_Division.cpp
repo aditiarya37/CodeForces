6c9584a15060f51833cf9012885f5ac4420f4ece
aditiarya37 <aditiarya1511@gmail.com>
2025-04-25 23:24:30 +0530

    #83 done

---
+++ 51-100 Problems/83_Division.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, rating;
+    cin >> t;
+
+    while (t--) {
+        cin >> rating;
+        if (rating >= 1900) {
+            cout << "Division 1" << endl;
+        } else if (rating >= 1600) {
+            cout << "Division 2" << endl;
+        } else if (rating >= 1400) {
+            cout << "Division 3" << endl;
+        } else {
+            cout << "Division 4" << endl;
+        }
+    }
+
+    return 0;
+}
