4d6cfb79be6722309d073eeaa3e612c623e71e2b
aditiarya37 <aditiarya1511@gmail.com>
2025-05-01 23:42:16 +0530

    #89 done

---
+++ 51-100 Problems/89_InterestingDrink.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> prices(n);
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+    sort(prices.begin(), prices.end());
+    int q;
+    cin >> q;
+    while (q--) {
+        int money;
+        cin >> money;
+        int count = upper_bound(prices.begin(), prices.end(), money) - prices.begin();
+        cout << count << endl;
+    }
+    return 0;
+}
